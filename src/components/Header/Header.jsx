5db7a55e91f26998d172eba1c3e639b885c36bff
comconToukir <ahmdtoukir@gmail.com>
2022-09-29 17:50:16 +0600

    minor adjustments

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,8 @@
 const Header = () => {
   return (
     <header className="flex items-center gap-5 py-4 w-5/6 mx-auto">
-        <img className="w-12" src="favicon.ico" alt="" />
-        <h1 className="text-4xl text-title-clr font-bold font-montserrat tracking-wide">AFTER SCHOOL CLUB</h1>
+        <img className="w-10 md:w-12" src="favicon.ico" alt="" />
+        <h1 className="text-xl sm:text-2xl md:text-4xl  text-title-clr font-bold font-montserrat tracking-wide">AFTER SCHOOL CLUB</h1>
     </header>
   );
 };
